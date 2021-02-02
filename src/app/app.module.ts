aceb3836add225ff16593452434d5d5300d037c8
Sergio <sepi_147@hotmail.com>
2021-02-01 22:59:08 -0500

    Agrego componentes nav y clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
